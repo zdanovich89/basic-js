5404b3793974a1ee8385b2bd3eb83292491dc839
zdanovich89 <zdanovich89@gmail.com>
2020-03-22 23:16:37 +0300

    feat: add solution 'carbon-dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,21 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+    
+    if (typeof sampleActivity != "string") {   
+        return false;
+    }
+
+    let activityNumber = sampleActivity.match(/^\d+(\.\d+)?/g);
+    if ((!activityNumber) ||
+        (Number(activityNumber[0]) > 15) ||
+        (Number(activityNumber[0]) <= 0)) {
+        return false;
+    }
+
+
+    
+    return Math.ceil(Math.log((MODERN_ACTIVITY / Number(activityNumber[0]))) / (0.693 / HALF_LIFE_PERIOD));
 };
